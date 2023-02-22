d649748c7b74e7d3e26d02b1cd0f102f56989771
Joao Gosmani <joaogosmani@gmail.com>
2023-02-22 16:23:05 -0300

    Criando a validação de e-mail único

---
+++ src/main/java/br/com/joaogosmani/hyperprof/api/professores/validators/ProfessorEmailIsUniqueValidator.java
@@ -0,0 +1,24 @@
+package br.com.joaogosmani.hyperprof.api.professores.validators;
+
+import org.springframework.stereotype.Component;
+
+import br.com.joaogosmani.hyperprof.core.repositories.ProfessorRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ProfessorEmailIsUniqueValidator implements ConstraintValidator<ProfessorEmailIsUnique, String> {
+
+    private final ProfessorRepository professorRepository;
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+        return !professorRepository.existsByEmail(value); 
+    }
+    
+}
