25f140ef40bd46cb3f46416f78223c66dfa312c9
Joao Gosmani <joaogosmani@gmail.com>
2023-02-22 13:53:38 -0300

    Criando o dto ProfessorRequest e o mapper toProfessor

--- src/main/java/br/com/joaogosmani/hyperprof/api/professores/mappers/ProfessorMapper.java
+++ src/main/java/br/com/joaogosmani/hyperprof/api/professores/mappers/ProfessorMapper.java
@@ -1,10 +1,12 @@
 package br.com.joaogosmani.hyperprof.api.professores.mappers;
 
+import br.com.joaogosmani.hyperprof.api.professores.dtos.ProfessorRequest;
 import br.com.joaogosmani.hyperprof.api.professores.dtos.ProfessorResponse;
 import br.com.joaogosmani.hyperprof.core.models.Professor;
 
 public interface ProfessorMapper {
     
+    Professor toProfessor(ProfessorRequest professorRequest);
     ProfessorResponse toProfessorResponse(Professor professor);
 
 }
