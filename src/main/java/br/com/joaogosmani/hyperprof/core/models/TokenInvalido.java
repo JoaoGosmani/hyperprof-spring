03672185c1a7a354efa20b708a968aefe284d685
Joao Gosmani <joaogosmani@gmail.com>
2023-03-10 12:31:47 -0300

    Criando o modelo de TokenInvalido

---
+++ src/main/java/br/com/joaogosmani/hyperprof/core/models/TokenInvalido.java
@@ -0,0 +1,33 @@
+package br.com.joaogosmani.hyperprof.core.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tokens_invalidos")
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+public class TokenInvalido extends Auditable {
+    
+    @Id
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "token")
+    private String token;
+
+}
